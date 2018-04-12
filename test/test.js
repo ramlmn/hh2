ae4f0d23f2c6f6c7ea7f8ef897849986b048a12d
chatbot <ramdamera@outlook.com>
2018-04-13 00:35:16 +0530

    Adapt tests to API changes, add http2 test

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const path = require('path');
+const http2 = require('http2');
 const {TLSSocket} = require('tls');
 const {promisify} = require('util');
 const request = promisify(require('request').defaults({strictSSL: false}));
@@ -8,7 +8,6 @@ const request = promisify(require('request').defaults({strictSSL: false}));
 const tape = require('tape');
 const tapSpec = require('tap-spec');
 
-const express = require('express');
 const pem = require('pem');
 const createCerts = promisify(pem.createCertificate);
 const getPort = require('get-port');
@@ -27,17 +26,23 @@ const getCerts = async _ => {
   };
 };
 
+const {
+  HTTP2_HEADER_STATUS,
+  HTTP2_HEADER_PATH,
+} = http2.constants;
+
 tape.createStream()
   .pipe(tapSpec())
   .pipe(process.stdout);
 
 
-const app = require('express')();
-app.use(express.static(path.resolve(__dirname, './public')));
+const app = (req, res) => {
+  res.end('OK, go');
+};
 
-const isOK = (res) => res.statusCode === 200;
+const isOK = res => res.statusCode === 200;
 
-const usesSSL = (res) => {
+const usesSSL = res => {
   return (isOK(res) && (res.socket instanceof TLSSocket || res.connection instanceof TLSSocket));
 };
 
@@ -45,15 +50,13 @@ const usesSSL = (res) => {
 tape('http test', async t => {
   t.plan(1);
 
-  const server = hh2(app, {
-    type: 'http',
-  });
+  const server = hh2(app);
 
   const port = await getPort();
 
   server.listen(port, '::', async _ => {
     try {
-      const res = await request(`http://localhost:${port}/sample.json`);
+      const res = await request(`http://localhost:${port}`);
 
       if (isOK(res)) {
         t.pass(`http mode working, status: ${res.statusCode}`);
@@ -75,7 +78,7 @@ tape('https test', async t => {
   const certs = await getCerts();
 
   const server = hh2(app, {
-    type: 'https',
+    secure: true,
     key: certs.key,
     cert: certs.cert,
   });
@@ -84,7 +87,7 @@ tape('https test', async t => {
 
   server.listen(port, '::', async _ => {
     try {
-      const res = await request(`https://localhost:${port}/sample.json`, {insecure: true});
+      const res = await request(`https://localhost:${port}`, {insecure: true});
 
       if (usesSSL(res)) {
         t.pass(`https mode working, status: ${res.statusCode}`);
@@ -100,4 +103,46 @@ tape('https test', async t => {
 });
 
 
-// @TODO: add http2 test
+tape('http2 test', async t => {
+  t.plan(1);
+
+  const certs = await getCerts();
+
+  const server = hh2(app, {
+    h2: true,
+    secure: true,
+    key: certs.key,
+    cert: certs.cert,
+  });
+
+  const port = await getPort();
+
+  server.listen(port, '::', async _ => {
+    const client = http2.connect(`https://localhost:${port}`, {
+      ca: [certs.cert],
+    });
+
+    client.on('error', e => t.fail('Failed: ', e));
+    client.on('close', _ => false);
+
+    const req = client.request({[HTTP2_HEADER_PATH]: '/'});
+
+    req.setEncoding('utf8');
+
+    req.on('response', headers => {
+      let data = '';
+      req.on('data', chunk => data += chunk);
+
+      req.on('end', async _ => {
+        const statusCode = headers[HTTP2_HEADER_STATUS];
+        if (statusCode && statusCode === 200) {
+          t.pass(`http2 over TLS ok, status: ${statusCode}`);
+        } else {
+          t.fail(`http2 over TLS not ok, status: ${statusCode}`);
+        }
+        client.close();
+        server.close();
+      });
+    });
+  });
+});
